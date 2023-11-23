e36a8fb28f8b5a2c10a927a2d675bf5c36f80281
tolga budanoğlu <budanoglu.tolga@gmail.com>
2023-11-23 19:57:12 +0300

    init

---
+++ Odev7ToDoApp/app/build.gradle.kts
@@ -0,0 +1,54 @@
+plugins {
+    id("com.android.application")
+    id("androidx.navigation.safeargs")
+    id("dagger.hilt.android.plugin")
+}
+
+android {
+    namespace = "com.example.odev7todoapp"
+    compileSdk = 34
+
+    defaultConfig {
+        applicationId = "com.example.odev7todoapp"
+        minSdk = 26
+        targetSdk = 34
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+}
+
+dependencies {
+
+    implementation("androidx.appcompat:appcompat:1.6.1")
+    implementation("com.google.android.material:material:1.10.0")
+    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+    testImplementation("junit:junit:4.13.2")
+    androidTestImplementation("androidx.test.ext:junit:1.1.5")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+
+    implementation ("androidx.lifecycle:lifecycle-viewmodel:2.5.1")
+
+    implementation("com.google.dagger:hilt-android:2.44")
+    annotationProcessor("com.google.dagger:hilt-android-compiler:2.44")
+
+    implementation ("androidx.room:room-runtime:2.4.3")
+    annotationProcessor ("androidx.room:room-compiler:2.4.3")
+    implementation ("androidx.room:room-rxjava2:2.4.3")
+    implementation ("io.reactivex.rxjava2:rxandroid:2.0.1")
+}
