346a3bb355d18e324c99b9b6006010a9633a7650
tolga budanoğlu <budanoglu.tolga@gmail.com>
2023-11-24 15:56:10 +0300

    dagger hilt eklendi

--- Odev7ToDoApp/app/src/main/java/com/example/odev7todoapp/di/AppModule.java
+++ Odev7ToDoApp/app/src/main/java/com/example/odev7todoapp/di/AppModule.java
@@ -1,4 +1,34 @@
 package com.example.odev7todoapp.di;
 
+import android.content.Context;
+
+import androidx.room.Room;
+
+import com.example.odev7todoapp.data.repo.ToDoDaoRepository;
+import com.example.odev7todoapp.room.Database;
+import com.example.odev7todoapp.room.ToDoDao;
+
+import javax.inject.Singleton;
+
+import dagger.Module;
+import dagger.Provides;
+import dagger.hilt.InstallIn;
+import dagger.hilt.android.qualifiers.ApplicationContext;
+import dagger.hilt.components.SingletonComponent;
+
+@Module
+@InstallIn(SingletonComponent.class)
 public class AppModule {
+    @Provides
+    @Singleton
+    public ToDoDaoRepository provideToDoDaoRepository(ToDoDao tDao){
+        return new ToDoDaoRepository(tDao);
+    }
+    @Provides
+    @Singleton
+    public ToDoDao provideToDao(@ApplicationContext Context context){
+        Database database = Room.databaseBuilder(context,Database.class,"toDoDatabase.sqlite")
+                .createFromAsset("toDoDatabase.sqlite").build();
+        return database.getToDoDao();
+    }
 }
--- Odev7ToDoApp/app/src/main/java/com/example/odev7todoapp/di/HiltApplication.java
+++ Odev7ToDoApp/app/src/main/java/com/example/odev7todoapp/di/HiltApplication.java
@@ -1,4 +1,9 @@
 package com.example.odev7todoapp.di;
 
-public class HiltApplication {
+import android.app.Application;
+
+import dagger.hilt.android.HiltAndroidApp;
+
+@HiltAndroidApp
+public class HiltApplication extends Application {
 }
