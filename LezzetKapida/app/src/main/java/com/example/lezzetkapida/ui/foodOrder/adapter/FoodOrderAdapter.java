9b75174e006e19ef3214d1c6da0f4d55489bae37
tolga budanoğlu <budanoglu.tolga@gmail.com>
2023-11-29 18:41:33 +0300

    sepet adapter yazıldı

--- LezzetKapida/app/src/main/java/com/example/lezzetkapida/ui/foodOrder/adapter/FoodOrderAdapter.java
+++ LezzetKapida/app/src/main/java/com/example/lezzetkapida/ui/foodOrder/adapter/FoodOrderAdapter.java
@@ -1,4 +1,62 @@
 package com.example.lezzetkapida.ui.foodOrder.adapter;
 
-public class FoodOrderAdapter {
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.lezzetkapida.data.entity.Food;
+import com.example.lezzetkapida.data.entity.FoodBasket;
+import com.example.lezzetkapida.databinding.FoodOrderRowLayoutBinding;
+import com.example.lezzetkapida.databinding.HomeRowLayoutBinding;
+import com.example.lezzetkapida.ui.home.adapter.HomeAdapter;
+import com.example.lezzetkapida.ui.viewModel.FoodOrderViewModel;
+import com.example.lezzetkapida.ui.viewModel.HomeViewModel;
+
+import java.util.List;
+
+public class FoodOrderAdapter extends RecyclerView.Adapter<FoodOrderAdapter.FoodBasketViewHolder> {
+
+    private Context mcontext;
+    private FoodOrderViewModel viewModel;
+
+    private List<FoodBasket> foodBasketList;
+
+    public FoodOrderAdapter(Context mcontext, FoodOrderViewModel viewModel, List<FoodBasket> foodBasketList) {
+        this.mcontext = mcontext;
+        this.viewModel = viewModel;
+        this.foodBasketList = foodBasketList;
+    }
+    public class FoodBasketViewHolder extends RecyclerView.ViewHolder{
+        private FoodOrderRowLayoutBinding binding;
+
+        public FoodBasketViewHolder(FoodOrderRowLayoutBinding binding) {
+            super(binding.getRoot());
+            this.binding = binding;
+        }
+    }
+
+    @NonNull
+    @Override
+    public FoodBasketViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        FoodOrderRowLayoutBinding binding = FoodOrderRowLayoutBinding.inflate(LayoutInflater.from(mcontext),parent,false);
+        return new FoodOrderAdapter.FoodBasketViewHolder(binding);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull FoodBasketViewHolder holder, int position) {
+
+        FoodBasket foodBasket = foodBasketList.get(position);
+        FoodOrderRowLayoutBinding binding = holder.binding;
+
+
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return foodBasketList.size();
+    }
 }
--- LezzetKapida/app/src/main/java/com/example/lezzetkapida/ui/viewModel/FoodOrderViewModel.java
+++ LezzetKapida/app/src/main/java/com/example/lezzetkapida/ui/viewModel/FoodOrderViewModel.java
@@ -1,4 +1,26 @@
 package com.example.lezzetkapida.ui.viewModel;
 
-public class FoodOrderViewModel {
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+import com.example.lezzetkapida.data.entity.Food;
+import com.example.lezzetkapida.data.entity.FoodBasket;
+import com.example.lezzetkapida.data.repo.FoodRepository;
+
+import java.util.List;
+
+import javax.inject.Inject;
+
+import dagger.hilt.android.AndroidEntryPoint;
+
+@AndroidEntryPoint
+public class FoodOrderViewModel extends ViewModel {
+    public FoodRepository frepo;
+    public MutableLiveData<List<FoodBasket>> foodBasketList;
+
+    @Inject
+    public FoodOrderViewModel(FoodRepository frepo, MutableLiveData<List<FoodBasket>> foodBasketList) {
+        this.frepo = frepo;
+        this.foodBasketList = foodBasketList;
+    }
 }
